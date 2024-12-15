e43ab4b28a6229c5891072b8954d19134dda2250
Arthur L Jones <arthur.l.jones@gmail.com>
2024-12-14 18:17:40 -0800

    Pull settings selectors into separate methods

--- app/src/main/java/tech/ajones/forcebuilder/ui/composable/GenerationOptions.kt
+++ app/src/main/java/tech/ajones/forcebuilder/ui/composable/GenerationOptions.kt
@@ -30,82 +30,113 @@ fun GenerationOptions(
   onRandomizeTap: () -> Unit
 ) {
   Column {
-    val settings by settingsSource.collectAsStateWithLifecycle()
     Text("Settings", style = MaterialTheme.typography.titleLarge)
-    MultiChoiceSegmentedButtonRow {
-      val items = MiniLibrary.entries
-      items.forEachIndexed { index, library ->
-        SegmentedButton(
-          checked = settings.library == library,
-          onCheckedChange = { settingsSource.update { it.copy(library = library) } },
-          SegmentedButtonDefaults.itemShape(index = index, count = items.size),
-        ) {
-          Text(text = library.name)
-        }
-      }
-    }
-    MultiChoiceSegmentedButtonRow {
-      val items = TechBase.entries
-      items.forEachIndexed { index, tech ->
-        SegmentedButton(
-          checked = settings.techBase.contains(tech),
-          onCheckedChange = { checked ->
-            settingsSource.update {
-              val newVal = if (checked) it.techBase + tech else it.techBase - tech
-              it.copy(techBase = newVal)
-            }
-          },
-          SegmentedButtonDefaults.itemShape(index = index, count = items.size),
-        ) {
-          Text(text = tech.name)
-        }
-      }
-    }
-    val max = 800
-    val step = 10
-    Slider(
-      value = settings.maxPointsValue.toFloat(),
-      onValueChange = { newPointsMax ->
-        settingsSource.update { it.copy(maxPointsValue = newPointsMax.roundToInt()) }
-      },
-      steps = max / step - 1,
-      valueRange = 0f..max.toFloat()
-    )
-    IntField(
-      value = settings.maxPointsValue,
-      label = { Text("Max PV") },
-      onValueChange = { value ->
-        settingsSource.update {
-          it.copy(maxPointsValue = value ?: 0)
-        }
-      }
-    )
-
-    Row {
-      IntField(
-        value = settings.minUnits,
-        label = { Text("Min Units") },
-        modifier = Modifier.weight(1f),
-        onValueChange = { newValue ->
-          settingsSource.update { it.copy(minUnits = newValue) }
-        },
-      )
-      Spacer(modifier = Modifier.size(4.dp))
-      IntField(
-        value = settings.maxUnits,
-        label = { Text("Max Units") },
-        modifier = Modifier.weight(1f),
-        onValueChange = { newValue ->
-          settingsSource.update { it.copy(maxUnits = newValue) }
-        },
-      )
-    }
+    
+    LibrarySelector(settingsSource = settingsSource)
+    TechBaseSelector(settingsSource = settingsSource)
+    MaxPVSelector(settingsSource = settingsSource)
+    MinMaxUnitsSelector(settingsSource = settingsSource)
 
     Button(
       onClick = { onRandomizeTap() }
     ) {
       Text("Randomize")
     }
+  }
+}
+
+@Composable
+private fun LibrarySelector(
+  settingsSource: MutableStateFlow<ForceSettings>
+) {
+  val settings by settingsSource.collectAsStateWithLifecycle()
+  MultiChoiceSegmentedButtonRow {
+    val items = MiniLibrary.entries
+    items.forEachIndexed { index, library ->
+      SegmentedButton(
+        checked = settings.library == library,
+        onCheckedChange = { settingsSource.update { it.copy(library = library) } },
+        SegmentedButtonDefaults.itemShape(index = index, count = items.size),
+      ) {
+        Text(text = library.name)
+      }
+    }
+  }
+}
+
+@Composable
+private fun TechBaseSelector(
+  settingsSource: MutableStateFlow<ForceSettings>
+) {
+  val settings by settingsSource.collectAsStateWithLifecycle()
+  MultiChoiceSegmentedButtonRow {
+    val items = TechBase.entries
+    items.forEachIndexed { index, tech ->
+      SegmentedButton(
+        checked = settings.techBase.contains(tech),
+        onCheckedChange = { checked ->
+          settingsSource.update {
+            val newVal = if (checked) it.techBase + tech else it.techBase - tech
+            it.copy(techBase = newVal)
+          }
+        },
+        SegmentedButtonDefaults.itemShape(index = index, count = items.size),
+      ) {
+        Text(text = tech.name)
+      }
+    }
+  }
+}
+
+@Composable
+private fun MaxPVSelector(
+  settingsSource: MutableStateFlow<ForceSettings>
+) {
+  val settings by settingsSource.collectAsStateWithLifecycle()
+  val max = 800
+  val step = 10
+  Slider(
+    value = settings.maxPointsValue.toFloat(),
+    onValueChange = { newPointsMax ->
+      settingsSource.update { it.copy(maxPointsValue = newPointsMax.roundToInt()) }
+    },
+    steps = max / step - 1,
+    valueRange = 0f..max.toFloat()
+  )
+  IntField(
+    value = settings.maxPointsValue,
+    label = { Text("Max PV") },
+    onValueChange = { value ->
+      settingsSource.update {
+        it.copy(maxPointsValue = value ?: 0)
+      }
+    }
+  )
+}
+
+@Composable
+private fun MinMaxUnitsSelector(
+  settingsSource: MutableStateFlow<ForceSettings>
+) {
+  val settings by settingsSource.collectAsStateWithLifecycle()
+  Row {
+    IntField(
+      value = settings.minUnits,
+      label = { Text("Min Units") },
+      modifier = Modifier.weight(1f),
+      onValueChange = { newValue ->
+        settingsSource.update { it.copy(minUnits = newValue) }
+      },
+    )
+    Spacer(modifier = Modifier.size(4.dp))
+    IntField(
+      value = settings.maxUnits,
+      label = { Text("Max Units") },
+      modifier = Modifier.weight(1f),
+      onValueChange = { newValue ->
+        settingsSource.update { it.copy(maxUnits = newValue) }
+      },
+    )
   }
 }
 
